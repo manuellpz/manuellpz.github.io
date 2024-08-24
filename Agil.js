d8d3816c2c8dbb16f490fbd08b7df731ffc6ab7c
manuellpz <54088833+manuellpz@users.noreply.github.com>
2024-08-23 17:09:46 -0700

    Update Agil.js

--- Agil.js
+++ Agil.js
@@ -19,51 +19,52 @@ const setStyles = (element, styles) => {
 
 /* CONVERTIR DECIMAL A ROMANO */
 const toRoman = (number) => {
-	let arrNum = number
-		.toString()
-		.split('')
-		.map((el) => parseInt(el));
-	let romans = {
-		0: '',
-		3000: 'MMM',
-		2000: 'MM',
-		1000: 'M',
-		900: 'CM',
-		800: 'DCCC',
-		700: 'DCC',
-		600: 'DC',
-		500: 'D',
-		400: 'CD',
-		300: 'CCC',
-		200: 'CC',
-		100: 'C',
-		90: 'XC',
-		80: 'LXXX',
-		70: 'LXX',
-		60: 'LX',
-		50: 'L',
-		40: 'XL',
-		30: 'XXX',
-		20: 'XX',
-		10: 'X',
-		9: 'IX',
-		8: 'VIII',
-		7: 'VII',
-		6: 'VI',
-		5: 'V',
-		4: 'IV',
-		3: 'III',
-		2: 'II',
-		1: 'I',
-	};
+  let arrNum = number
+    .toString()
+    .split('')
+    .map((el) => parseInt(el));
+  let romans = {
+    0: '',
+    3000: 'MMM',
+    2000: 'MM',
+    1000: 'M',
+    900: 'CM',
+    800: 'DCCC',
+    700: 'DCC',
+    600: 'DC',
+    500: 'D',
+    400: 'CD',
+    300: 'CCC',
+    200: 'CC',
+    100: 'C',
+    90: 'XC',
+    80: 'LXXX',
+    70: 'LXX',
+    60: 'LX',
+    50: 'L',
+    40: 'XL',
+    30: 'XXX',
+    20: 'XX',
+    10: 'X',
+    9: 'IX',
+    8: 'VIII',
+    7: 'VII',
+    6: 'VI',
+    5: 'V',
+    4: 'IV',
+    3: 'III',
+    2: 'II',
+    1: 'I',
+  };
 
-	let result = '';
+  let result = '';
+  let mult = [1, 10, 100, 1000];
+  let id = arrNum.length - 1;
+  
+  for (let i = 0; i < arrNum.length; i++) {
+    result += romans[arrNum[i] * mult[id]];
+    id--;
+  }
 
-	result =
-		romans[arrNum[0] * 1000] +
-		romans[arrNum[1] * 100] +
-		romans[arrNum[2] * 10] +
-		romans[arrNum[3]];
-
-	return result;
+  return result;
 };
